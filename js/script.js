6be30697cbad208ef84fcafbe46b67f7d286b0c6
Xudko04031990 <kostya040390@gmail.com>
2019-02-11 19:03:26 +0200

    add styles and some logic

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 
 var hoursArrow = document.querySelector('.clock__hours'),
 minutesArrow = document.querySelector('.clock__minutes'),
-secondsArrow = document.querySelector('.clock__seconds');
+secondsArrow = document.querySelector('.clock__seconds'),
+digitalHours = document.querySelector('.clock__digital-hours'),
+digitalMinutes = document.querySelector('.clock__digital-minutes'),
+digitalSeconds = document.querySelector('.clock__digital-seconds');
 
 function setDate() {
 
@@ -10,17 +13,18 @@ function setDate() {
     
     var seconds = current.getSeconds();
     var secondsDegree = ((seconds / 60)*360) + 90;
-    secondsArrow.style.transform = 'rotate(' + secondsDegree + 'deg)';
+    secondsArrow.style.transform = 'translateY(-50%) rotate(' + secondsDegree + 'deg)';
+    digitalSeconds.innerHTML = (seconds < 10) ? '0' + seconds : seconds;
 
     var minutes = current.getMinutes();
     var minutesDegree = ((minutes / 60)*360) + 90;
-    console.log(minutes);
-    console.log(minutesDegree);
-    minutesArrow.style.transform = 'rotate(' + minutesDegree + 'deg)';
+    minutesArrow.style.transform = 'translateY(-50%) rotate(' + minutesDegree + 'deg)';
+    digitalMinutes.innerHTML = (minutes < 10) ? '0' + minutes : minutes;
 
     var hours = current.getHours();
     var hoursDegree = ((hours / 12)*360) + 90;
-    hoursArrow.style.transform = 'rotate(' + hoursDegree + 'deg)';
+    hoursArrow.style.transform = 'translateY(-50%) rotate(' + hoursDegree + 'deg)';
+    digitalHours.innerHTML = (hours < 10) ? '0' + hours : hours;
 }
 
 setInterval(setDate, 1000);
